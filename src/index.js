97c7bf7bf9e3ebfd5b106903912f89c1f9d36e84
Ryan Kelley <rtkelley@gmail.com>
2017-11-20 13:12:37 -0500

    updated index file and created redux folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
+//react imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+//router imports
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+//redux imports
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+
+//import styles
+import './styles/index.css';
+import './styles/bootstrap.min.css';
+
+//import components
 import App from './views/App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//create store for redux and apply applyMiddleware
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <BrowserRouter>
+      <BaseLayout>
+        <Switch>
+          <Route exact path = "/" component={App} />
+        </Switch>
+      </BaseLayout>
+    </BrowserRouter>
+  </Provider>
+
+  , document.getElementById('root'));
+  registerServiceWorker();
